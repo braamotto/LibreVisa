d13edd7872763409ce81a7ddefa8fddf3369c911
Simon Richter <Simon.Richter@hogyros.de>
2012-12-07 22:16:06 +0000

    Add test for rule 3.6.38
    This rule specifies that unlocking an object that is not locked gives an
    error.

---
+++ tests/rule_3_6_38.c
@@ -0,0 +1,22 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <visa.h>
+
+int main()
+{
+	ViSession vi;
+
+	ViStatus rc = viOpenDefaultRM(&vi);
+
+	if(rc != VI_SUCCESS)
+		return 1;
+
+	rc = viUnlock(vi);
+
+	if(rc != VI_ERROR_SESN_NLOCKED)
+		return 1;
+
+	return 0;
+}
