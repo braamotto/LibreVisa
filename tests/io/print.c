aa9970f4fde7599feb51821ac8fb617550aa55de
A. Pic <dev@apic.name>
2012-12-11 12:45:28 +0100

    Add dummy_resource to print test, fix buffer type

--- tests/io/print.c
+++ tests/io/print.c
@@ -5,22 +5,40 @@
 #include <visa.h>
 #include <stdio.h>
 
+#include "dummy.h"
+
 int main()
 {
+        using_dummy_resource();
+
         ViSession rm;
         ViStatus rc = viOpenDefaultRM(&rm);
 
         if(rc != VI_SUCCESS)
                 return 1;
-        if(rm == VI_NULL)
+
+        ViSession dummySession;
+
+        rc = viOpen(rm, "DUMMY", VI_NO_LOCK, 0, &dummySession);
+
+        if(rc != VI_SUCCESS)
                 return 1;
 
-        viSetBuf(rm, VI_IO_OUT_BUF, 42);
-        viPrintf(rm, "foobar\\nbaz");
+        if(dummySession == VI_NULL)
+                return 1;
+
+        if(dummySession == rm)
+                return 1;
+
+        viSetBuf(dummySession, VI_WRITE_BUF, 42);
+        viPrintf(dummySession, "foobar\\nbaz");
 
         // @todo Do a debug output of buffer contents
 
         viClose(rm);
+
+        if(rc != VI_SUCCESS)
+                return 1;
 
         return 0;
 }
