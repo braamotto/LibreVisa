7d1f55e2421b8f35ec13602b07f7144afda1cdd6
Simon Richter <Simon.Richter@hogyros.de>
2012-12-28 15:09:22 +0000

    Start work on VXI interrupt channel

--- src/vxi.cpp
+++ src/vxi.cpp
@@ -13,9 +13,16 @@
 
 #include <string>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 #include <rpc/rpc.h>
 
 #include <vxi.h>
+#include <vxi_intr.h>
+
+extern "C" void device_intr_1(struct svc_req *rqstp, register SVCXPRT *transp);
 
 namespace {
 
@@ -28,6 +35,11 @@ bool is_valid_in_hostname(char c)
                 (c == '-') || (c == '.');
 }
 
+}
+
+void *device_intr_srq_1_svc(Device_SrqParms *, svc_req *)
+{
+        return 0;
 }
 
 namespace freevisa {
@@ -62,7 +74,41 @@ private:
 vxi_resource::vxi_resource(std::string const &hostname) :
         io_timeout(10), lock_timeout(10)
 {
-        client = clnt_create(hostname.c_str(), DEVICE_CORE, DEVICE_CORE_VERSION, "tcp");
+        int sock = RPC_ANYSOCK;
+
+        addrinfo hints =
+        {
+                AI_ADDRCONFIG,
+                AF_INET,
+                SOCK_STREAM,
+                IPPROTO_TCP,
+                0,
+                0,
+                0,
+                0
+        };
+
+        addrinfo *results;
+
+        int rc = getaddrinfo(
+                hostname.c_str(),
+                0,
+                &hints,
+                &results);
+        if(rc != 0)
+                throw exception(VI_ERROR_RSRC_NFOUND);
+
+        for(addrinfo *i = results; i; i = i->ai_next)
+        {
+                if(i->ai_addr->sa_family != AF_INET)
+                        continue;
+
+                sockaddr_in *sin = reinterpret_cast<sockaddr_in *>(i->ai_addr);
+                sin->sin_port = 0;
+                client = clnttcp_create(sin, DEVICE_CORE, DEVICE_CORE_VERSION, &sock, 0, 0);
+                if(client)
+                        break;
+        }
 
         if(!client)
                 throw exception(VI_ERROR_RSRC_NFOUND);
@@ -72,6 +118,43 @@ vxi_resource::vxi_resource(std::string const &hostname) :
 
         if(!resp)
                 throw exception(VI_ERROR_RSRC_NFOUND);
+
+        SVCXPRT *intr_svc = svcudp_create(RPC_ANYSOCK);
+
+        if(!intr_svc)
+                throw exception(VI_ERROR_SYSTEM_ERROR);
+
+        if(!svc_register(intr_svc, DEVICE_ASYNC, DEVICE_ASYNC_VERSION, device_intr_1, IPPROTO_UDP))
+                throw exception(VI_ERROR_SYSTEM_ERROR);
+
+        sockaddr_storage ss;
+
+        socklen_t ss_len = sizeof ss;
+        rc = getsockname(sock, reinterpret_cast<sockaddr *>(&ss), &ss_len);
+        if(rc == -1)
+                throw exception(VI_ERROR_SYSTEM_ERROR);
+
+        if(ss.ss_family != AF_INET)
+                throw exception(VI_ERROR_SYSTEM_ERROR);
+
+        sockaddr_in *sin = reinterpret_cast<sockaddr_in *>(&ss);
+
+        Device_RemoteFunc rfn =
+        {
+                ntohl(sin->sin_addr.s_addr),
+                ntohs(sin->sin_port),
+                DEVICE_ASYNC,
+                DEVICE_ASYNC_VERSION,
+                DEVICE_UDP
+        };
+
+        Device_Error *error = create_intr_chan_1(&rfn, client);
+
+        if(!error)
+                throw exception(VI_ERROR_SYSTEM_ERROR);
+
+        if(error->error)
+                throw exception(VI_ERROR_SYSTEM_ERROR);
 
         /// @todo handle errors
         lid = resp->lid;
