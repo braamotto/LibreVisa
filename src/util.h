0baa04d2d14727ee4567693729d67a13b3bc488a
Simon Richter <Simon.Richter@hogyros.de>
2012-12-28 15:10:14 +0000

    Start work on USB support

--- src/util.h
+++ src/util.h
@@ -18,6 +18,40 @@ inline unsigned int parse_optional_int(char const *&cursor)
         return ret;
 }
 
+inline unsigned int parse_hex(char const *&cursor)
+{
+        unsigned int ret = 0;
+        if(cursor[0] != '0' || cursor[1] != 'x')
+                return 0;
+
+        cursor += 2;
+
+        for(;;)
+        {
+                char const c = *cursor;
+
+                if(c >= '0' && c <= '9')
+                {
+                        ret <<= 4;
+                        ret += c - '0';
+                }
+                else if(c >= 'A' && c <= 'F')
+                {
+                        ret <<= 4;
+                        ret += c - 'A' + 10;
+                }
+                else if(c >= 'a' && c <= 'f')
+                {
+                        ret <<= 4;
+                        ret += c - 'a' + 10;
+                }
+                else
+                        return ret;
+
+                ++cursor;
+        }
+}
+
 }
 
 #endif
