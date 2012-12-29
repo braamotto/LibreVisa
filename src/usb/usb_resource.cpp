5cd1a6d325d4bb6d9b93bf62ad48aa48ff0bd051
Simon Richter <Simon.Richter@hogyros.de>
2012-12-29 01:42:30 +0000

    Implement USB ReadSTB

--- src/usb/usb_resource.cpp
+++ src/usb/usb_resource.cpp
@@ -9,7 +9,20 @@
 namespace freevisa {
 namespace usb {
 
-usb_resource::usb_resource(unsigned int vendor, unsigned int product, usb_string const &serial)
+enum
+{
+        READ_STATUS_BYTE = 128,
+        REN_CONTROL = 160,
+        GO_TO_LOCAL = 161,
+        LOCAL_LOCKOUT = 162
+};
+
+usb_resource::usb_resource(unsigned int vendor, unsigned int product, usb_string const &serial) :
+        interface(0),
+        int_in_ep(0x83),
+        status_tag(0),
+        io_timeout(1000),
+        have_interrupt_endpoint(true)
 {
         if(openusb_init(0, &openusb) != OPENUSB_SUCCESS)
                 throw exception(VI_ERROR_SYSTEM_ERROR);
@@ -51,10 +64,13 @@ usb_resource::usb_resource(unsigned int vendor, unsigned int product, usb_string
 
         if(openusb_open_device(openusb, devid, USB_INIT_DEFAULT, &dev) != OPENUSB_SUCCESS)
                 throw exception(VI_ERROR_SYSTEM_ERROR);
+        if(openusb_claim_interface(dev, interface, USB_INIT_DEFAULT) != OPENUSB_SUCCESS)
+                throw exception(VI_ERROR_SYSTEM_ERROR);
 }
 
 usb_resource::~usb_resource() throw()
 {
+        openusb_release_interface(dev, interface);
         openusb_close_device(dev);
         openusb_fini(openusb);
 }
@@ -62,6 +78,61 @@ usb_resource::~usb_resource() throw()
 ViStatus usb_resource::Close()
 {
         delete this;
+        return VI_SUCCESS;
+}
+
+ViStatus usb_resource::ReadSTB(ViUInt16 *retStatus)
+{
+        unsigned char buffer[3];
+
+        openusb_ctrl_request creq =
+        {
+                {
+                        USB_REQ_DEV_TO_HOST|USB_REQ_TYPE_CLASS|USB_REQ_RECIP_INTERFACE,
+                        READ_STATUS_BYTE,
+                        openusb_cpu_to_le16(status_tag),
+                        openusb_cpu_to_le16(interface),
+                },
+                buffer,
+                sizeof buffer,
+                io_timeout,
+                0,
+                { 0, 0 },
+                0
+        };
+
+        status_tag = (status_tag + 1) & 0x7f;
+
+        uint32_t rc = openusb_ctrl_xfer(dev, 0, 0, &creq);
+
+        if(rc != OPENUSB_SUCCESS)
+                throw exception(VI_ERROR_IO);
+
+        if(!have_interrupt_endpoint)
+        {
+                *retStatus = buffer[2];
+                return VI_SUCCESS;
+        }
+
+        unsigned char ibuffer[2];
+
+        openusb_intr_request ireq =
+        {
+                128,
+                ibuffer,
+                sizeof ibuffer,
+                io_timeout,
+                0,
+                { 0, 0 },
+                0
+        };
+
+        rc = openusb_intr_xfer(dev, interface, int_in_ep, &ireq);
+
+        if(rc != OPENUSB_SUCCESS)
+                throw exception(VI_ERROR_IO);
+
+        *retStatus = ibuffer[1];
         return VI_SUCCESS;
 }
 
--- src/usb/usb_resource.h
+++ src/usb/usb_resource.h
@@ -18,9 +18,15 @@ private:
         ~usb_resource() throw();
 
         virtual ViStatus Close();
+        virtual ViStatus ReadSTB(ViUInt16 *);
 
         openusb_handle_t openusb;
         openusb_dev_handle_t dev;
+        uint8_t interface;
+        uint8_t int_in_ep;
+        uint8_t status_tag;
+        unsigned int io_timeout;
+        bool have_interrupt_endpoint;
 
         class creator;
 };
