e0d53284f723bbed5b189090e8112e6926453b96
Simon Richter <Simon.Richter@hogyros.de>
2013-01-15 20:29:41 +0100

    Add condition variable to event queue
    This condition variable will be used to notify waiting threads of the
    arrival of new events.

--- src/condvar.h
+++ src/condvar.h
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2012 Simon Richter
+ * Copyright (C) 2013 Simon Richter
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,40 +15,21 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef librevisa_event_queue_h_
-#define librevisa_event_queue_h_ 1
+#include <pthread.h>
 
-#include "event.h"
-
-#include "mutex.h"
-#include "condvar.h"
+#ifndef librevisa_condvar_h_
+#define librevisa_condvar_h_ 1
 
 namespace librevisa {
 
-class event_queue
+class condvar
 {
 public:
-        event_queue(unsigned int size = 10);
-
-        typedef unsigned int size_type;
-
-        size_type get_size() { return size; }
-        void set_size(size_type size);
+        condvar() { pthread_cond_init(&impl, 0); }
+        ~condvar() throw() { pthread_cond_destroy(&impl); }
 
 private:
-        mutex cs;
-        condvar cv;
-
-        size_type size;
-        size_type readp;
-        size_type writep;
-
-        event::data *data;
-
-        size_type count() const
-        {
-                return (writep>readp)?(writep-readp):(size-readp+writep);
-        }
+        pthread_cond_t impl;
 };
 
 }
