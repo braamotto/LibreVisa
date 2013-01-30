1e4d2250f5546f13e3a579bc2f02aac49cb5be81
Simon Richter <Simon.Richter@hogyros.de>
2013-01-30 17:29:27 +0100

    Implement AVAHI scan

--- src/vxi/avahi.cpp
+++ src/vxi/avahi.cpp
@@ -22,36 +22,140 @@
 #include "avahi.h"
 
 #include "resource_manager.h"
+#include "findlist.h"
 
 #include "messagepump.h"
 
+#include <sstream>
+#include <algorithm>
 
 #include <avahi-client/client.h>
 #include <avahi-client/lookup.h>
 
+struct find_context
+{
+        librevisa::findlist *list;
+        AvahiClient *client;
+        std::list<AvahiServiceBrowser *> browsers;
+        std::list<AvahiServiceResolver *> resolvers;
+};
+
 extern "C" {
 static void avahi_client_callback(AvahiClient *, AvahiClientState, void *);
 static void avahi_service_browser_callback(AvahiServiceBrowser *, AvahiIfIndex, AvahiProtocol, AvahiBrowserEvent, const char *, const char *, const char *, AvahiLookupResultFlags, void *);
+static void avahi_service_resolver_callback(AvahiServiceResolver *r, AvahiIfIndex interface, AvahiProtocol protocol, AvahiResolverEvent event, const char *name, const char *type, const char *domain, const char *host_name, const AvahiAddress *a, uint16_t port, AvahiStringList *txt, AvahiLookupResultFlags flags, void *userdata);
 }
 
 static void avahi_client_callback(
-        AvahiClient *,
-        AvahiClientState,
-        void *)
+        AvahiClient *client,
+        AvahiClientState state,
+        void *userdata)
 {
+        find_context *ctx = reinterpret_cast<find_context *>(userdata);
+        switch(state)
+        {
+        case AVAHI_CLIENT_S_REGISTERING:
+        case AVAHI_CLIENT_S_RUNNING:
+        case AVAHI_CLIENT_S_COLLISION:
+                ctx->browsers.push_back(
+                        avahi_service_browser_new(
+                                client,
+                                AVAHI_IF_UNSPEC,
+                                AVAHI_PROTO_UNSPEC,
+                                "_vxi-11._tcp",
+                                0,
+                                AvahiLookupFlags(0),
+                                &avahi_service_browser_callback,
+                                userdata));
+                break;
+        case AVAHI_CLIENT_FAILURE:
+        case AVAHI_CLIENT_CONNECTING:
+                break;
+        }
 }
 
 static void avahi_service_browser_callback(
-        AvahiServiceBrowser *,
-        AvahiIfIndex,
-        AvahiProtocol,
-        AvahiBrowserEvent,
-        const char *,
-        const char *,
-        const char *,
+        AvahiServiceBrowser *browser,
+        AvahiIfIndex interface,
+        AvahiProtocol protocol,
+        AvahiBrowserEvent event,
+        const char *name,
+        const char *type,
+        const char *domain,
         AvahiLookupResultFlags,
-        void *)
+        void *userdata)
 {
+        find_context *ctx = reinterpret_cast<find_context *>(userdata);
+        switch(event)
+        {
+        case AVAHI_BROWSER_NEW:
+                ctx->resolvers.push_back(
+                        avahi_service_resolver_new(
+                                ctx->client,
+                                interface,
+                                protocol,
+                                name,
+                                type,
+                                domain,
+                                AVAHI_PROTO_UNSPEC,
+                                AvahiLookupFlags(0),
+                                &avahi_service_resolver_callback,
+                                userdata));
+                break;
+        case AVAHI_BROWSER_REMOVE:
+                break;
+        case AVAHI_BROWSER_CACHE_EXHAUSTED:
+        case AVAHI_BROWSER_ALL_FOR_NOW:
+        case AVAHI_BROWSER_FAILURE:
+                avahi_service_browser_free(browser);
+                ctx->browsers.remove(browser);
+                if(ctx->browsers.empty() && ctx->resolvers.empty())
+                {
+                        avahi_client_free(ctx->client);
+                        ctx->client = 0;
+                }
+                break;
+        }
+}
+
+static void avahi_service_resolver_callback(
+        AvahiServiceResolver *r,
+        AvahiIfIndex interface,
+        AvahiProtocol,
+        AvahiResolverEvent event,
+        const char *,
+        const char *,
+        const char *,
+        const char *,
+        const AvahiAddress *a,
+        uint16_t,
+        AvahiStringList *,
+        AvahiLookupResultFlags,
+        void *userdata)
+{
+        find_context *ctx = reinterpret_cast<find_context *>(userdata);
+        switch(event)
+        {
+        case AVAHI_RESOLVER_FAILURE:
+                break;
+
+        case AVAHI_RESOLVER_FOUND:
+                std::ostringstream os;
+                os << "TCPIP" << interface << "::";
+                char buffer[AVAHI_ADDRESS_STR_MAX];
+                avahi_address_snprint(buffer, sizeof buffer, a);
+                os << buffer << "::INSTR";
+                ctx->list->add(os.str().c_str());
+                break;
+        }
+
+        avahi_service_resolver_free(r);
+        ctx->resolvers.remove(r);
+        if(ctx->browsers.empty() && ctx->resolvers.empty())
+        {
+                avahi_client_free(ctx->client);
+                ctx->client = 0;
+        }
 }
 
 namespace librevisa {
@@ -69,24 +173,18 @@ avahi::~avahi() throw()
 
 void avahi::find(findlist &list) const
 {
-        AvahiClient *client = avahi_client_new(main, AvahiClientFlags(0), &avahi_client_callback, 0, 0);
-        if(!client)
+        find_context ctx;
+        ctx.list = &list;
+        ctx.client = avahi_client_new(main, AvahiClientFlags(0), &avahi_client_callback, &ctx, 0);
+        if(!ctx.client)
                 return;
-
-        AvahiServiceBrowser *browser = avahi_service_browser_new(
-                client,
-                AVAHI_IF_UNSPEC,
-                AVAHI_PROTO_UNSPEC,
-                "_lxi._tcp",
-                0,
-                AvahiLookupFlags(0),
-                avahi_service_browser_callback,
-                &list);
 
         main.run(5000);
 
-        avahi_service_browser_free(browser);
-        avahi_client_free(client);
+        //std::for_each(ctx.resolvers.begin(), ctx.resolvers.end(), &avahi_service_resolver_free);
+        std::for_each(ctx.browsers.begin(), ctx.browsers.end(), &avahi_service_browser_free);
+        if(ctx.client)
+                avahi_client_free(ctx.client);
 }
 
 avahi avahi::instance;
